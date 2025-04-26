7f6e729dd07ea5807f93f094b0251d1671f261cb
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-04-26 04:46:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,16 @@
 # Install Required Packages
 # ============================================
 
+    echo "\033[1;33m[+] Installing Required Packages...\033[0m"
+
     sudo xbps-install -Su
     sudo xbps-install -S vim make libX11-devel pkg-config libXft-devel libXinerama-devel glib-devel font-inconsolata-otf git xsetroot gcc make xorg-server xinit xrandr xorg xauth kitty -y 
 
 # ============================================
 # Set Up suckless directories
 # ============================================
+
+    echo "\033[1;33m[+] Set Up suckless directories...\033[0m"
 
     git clone https://github.com/emadadel4/dwm.git
     cd dwm/src/dwm
@@ -25,9 +29,13 @@
 # Set Up Xinitrc
 # ============================================
 
+    echo "\033[1;33m[+] Installing .xinitrc\033[0m"
+
     curl -o ~/.xinitrc https://raw.githubusercontent.com/emadadel4/dwm/refs/heads/main/src/scripts/.xinitrc
     chmod +x ~/.xinitrc
     
 # ============================================
 # END Set Up Xinitrc
 # ============================================
+
+echo -e "\n\033[32mInstallation completed!\033[0m"
