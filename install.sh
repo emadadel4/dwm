b220f66bed4e6983413322451c9fbee1b45282a9
Emad Adel <emadadel1920@gmail.com>
2025-04-27 02:58:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "\033[1;33m[+] Installing Required Packages...\033[0m"
 sudo xbps-install -Su -y
 sudo xbps-install -S vim make patch libX11-devel / 
 pkg-config libXft-devel libXinerama-devel glib-devel /
-font-inconsolata-otf git xsetroot gcc make xorg-server /
+font-inconsolata-otf git xsetroot gcc xorg-server /
 xinit xrandr xorg xauth kitty setxkbmap pipewire wireplumber /
 libspa-bluetooth sxhkd NetworkManager network-manager-applet dbus -y 
 # ============================================
