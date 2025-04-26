32f8fe4e7b171ae4c3d2781d926f251bddfde9a2
Emad Adel <emadadel1920@gmail.com>
2025-04-26 06:37:33 +0300

    Update config.def.h

--- src/dwm/config.def.h
+++ src/dwm/config.def.h
@@ -27,8 +27,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "thunar",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",    NULL,       NULL,       1 << 2,       0,           -1 },
+	{ NULL,     NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
