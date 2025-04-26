36668be5b372b40dfe3964692374e96e28a750c5
Emad Adel <emadadel1920@gmail.com>
2025-04-26 04:43:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,8 +25,8 @@
 # Set Up Xinitrc
 # ============================================
 
-    curl -o https://raw.githubusercontent.com/emadadel4/dwm/refs/heads/main/src/scripts/.xinitrc
-    chmod +x .xinitrc
+    curl -o ~/.xinitrc https://raw.githubusercontent.com/emadadel4/dwm/refs/heads/main/src/scripts/.xinitrc
+    chmod +x ~/.xinitrc
     
 # ============================================
 # END Set Up Xinitrc
