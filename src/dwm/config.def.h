5745e546701175a1b7a47897f9d3ef568b54a387
Emad Adel <emadadel1920@gmail.com>
2025-04-30 00:58:18 +0300

    Update config.def.h

--- src/dwm/config.def.h
+++ src/dwm/config.def.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *thunar[] = { "thunar", NULL };
 static const char *upvol[]      = { "wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "0.05%+",      NULL };
 static const char *downvol[]    = { "wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "0.05%-",      NULL };
